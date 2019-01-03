31a7cbaa14d6f40eba3f6a1ce4d524b8c40c9c62
yanggqm <289656703@qq.com>
2019-01-04 00:38:37 +0800

    process

---
+++ module/module_api.js
@@ -0,0 +1,4 @@
+// 其中name, data, getPrivate()为模块的api属性和方法
+
+var origin = require('./module_api_origin');
+console.log(origin.name, origin.data, origin.getPrivate());
---
+++ module/module_api_origin.js
@@ -0,0 +1,5 @@
+exports.name = 'yang';
+exports.data = 'this is data';
+
+var privateVar = 5;
+exports.getPrivate = () => privateVar;
