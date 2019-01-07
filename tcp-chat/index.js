dc5a77ff96ee57c7b8b877af48483e8bd4601e28
yangguoqiang01 <wochange@163.com>
2019-01-08 00:34:37 +0800

    add TCP

--- tcp-chat/index.js
+++ tcp-chat/index.js
@@ -1,9 +1,49 @@
 var net = require('net');
-
+// 和http协议表现形式一样
+// http协议是建立在tcp协议之上
+var count = 0,
+    users = {};
 var server = net.createServer(conn => {
-    console.log('new connetion')
+    console.log('新建tcp连接, 请输入您的nickname，并按下回车确认');
+    count++;
+    console.log('count计数器当前为:' + count);
+
+    conn.setEncoding('utf8');
+
+    var nickname;
+
+    conn.on('data', (data) => {
+        data = data.replace('\r\n', '');
+        // 接收到第一份数据应该是用户的昵称
+        if (!nickname) {
+            if (users[data]) {
+                conn.write('nickname 已经存在');
+                return;
+            }else {
+                nickname = data;
+                users[nickname] = conn;
+
+                for(let i in users) {
+                    users[i].write(nickname + '加入')
+                }
+            }
+        }else {
+            for (let i in users) {
+                // 否则，视为聊天信息
+                if (i != nickname) {
+                    users[i].write(nickname + ': ' + data);
+                }
+            } 
+        }
+    })
+
+    conn.on('close', () => {
+        count--
+        delete users[nickname]
+        console.log('count计数器当前为:' + count);
+    })
 })
 
 server.listen(3001, () => {
-    console.log('监听http: 3001端口')
+    console.log('监听http: 3001端口');
 })
